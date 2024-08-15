2543f47a3889910b5c7262746a8b16d5d47f1e8a
selizkol <56803513+selizkol@users.noreply.github.com>
2024-08-15 12:25:33 +0300

    Add files via upload

---
+++ VeeamZIP (HV)_HV1.ps1
@@ -0,0 +1,119 @@
+﻿# Author: Valery Sokolov
+# Created Date: 13/08/2024
+# 
+
+##################################################################
+#                   User Defined Variables
+##################################################################
+#FORFILES /p D:\backup\Servers\veam\tol_hv1\ /s /m *.* /d -1 /c "CMD /c del /Q @FILE"
+# Names of VMs to backup separated by comma (Mandatory). For instance, $VMNames = “VM1”,”VM2”
+$VMNames = "VM1","VM2","VM3"
+
+# Name of vCenter or standalone host VMs to backup reside on (Mandatory)
+$HostName = "HV1"
+
+# Directory that VM backups should go to (Mandatory; for instance, C:\Backup)
+$Directory = "C:\Backup\HV1\"
+
+# DelData is the number of days to store backups in the directory (required; for example, "3" days).
+$DelData = "3"
+
+# Desired compression level (Optional; Possible values: 0 - None, 4 - Dedupe-friendly, 5 - Optimal, 6 - High, 9 - Extreme) 
+$CompressionLevel = "5"
+
+# Quiesce VM when taking snapshot (Optional; VMware Tools are required; Possible values: $True/$False)
+$EnableQuiescence = $False
+# Protect resulting backup with encryption key (Optional; $True/$False)
+$EnableEncryption = $False
+
+# Encryption Key (Optional; path to a secure string)
+$EncryptionKey = ""
+
+# Retention settings (Optional; By default, VeeamZIP files are not removed and kept in the specified location for an indefinite period of time. 
+# Possible values: Never , Tonight, TomorrowNight, In3days, In1Week, In2Weeks, In1Month)
+$Retention = "Never"
+
+##################################################################
+#                   Notification Settings
+##################################################################
+
+# Enable notification (Optional)
+$EnableNotification = $True
+
+# Email SMTP server
+
+$SMTPServer = "mail.example.com"
+
+# Email FROM
+$EmailFrom = "hv1@example.com" 
+# Email TO
+$EmailTo = "my_mail@example.com"
+
+# Email subject
+$EmailSubject = "backup virtual servers HV1"
+$portSMTP = 25
+##################################################################
+#                   Email formatting 
+##################################################################
+
+$style = "<style>BODY{font-family: Arial; font-size: 10pt;}"
+$style = $style + "TABLE{border: 1px solid black; border-collapse: collapse;}"
+$style = $style + "TH{border: 1px solid black; background: #dddddd; padding: 5px; }"
+$style = $style + "TD{border: 1px solid black; padding: 5px; }"
+$style = $style + "</style>"
+
+##################################################################
+#                   End User Defined Variables
+##################################################################
+
+#################### DO NOT MODIFY PAST THIS LINE ################
+Asnp VeeamPSSnapin
+
+$Server = Get-VBRServer -name $HostName
+$MesssagyBody = @()
+
+foreach ($VMName in $VMNames)
+{
+  $VM = Find-VBRHvEntity -Name $VMName -Server $Server
+  
+  If ($EnableEncryption)
+  {
+    $EncryptionKey = Add-VBREncryptionKey -Password (cat $EncryptionKey | ConvertTo-SecureString)
+    $ZIPSession = Start-VBRZip -Entity $VM -Folder $Directory -Compression $CompressionLevel -DisableQuiesce:(!$EnableQuiescence) -AutoDelete $Retention -EncryptionKey $EncryptionKey
+  }
+  
+  Else 
+  {
+    $ZIPSession = Start-VBRZip -Entity $VM -Folder $Directory -Compression $CompressionLevel -DisableQuiesce:(!$EnableQuiescence) -AutoDelete $Retention
+  }
+  
+  If ($EnableNotification) 
+  {
+    $TaskSessions = $ZIPSession.GetTaskSessions().logger.getlog().updatedrecords
+    $FailedSessions =  $TaskSessions | where {$_.status -eq "EWarning" -or $_.Status -eq "EFailed"}
+  
+  if ($FailedSessions -ne $Null)
+  {
+    $MesssagyBody = $MesssagyBody + ($ZIPSession | Select-Object @{n="Name";e={($_.name).Substring(0, $_.name.LastIndexOf("("))}} ,@{n="Start Time";e={$_.CreationTime}},@{n="End Time";e={$_.EndTime}},Result,@{n="Details";e={$FailedSessions.Title}})
+  }
+   
+  Else
+  {
+  FORFILES /p $Directory /s /m *.* /d -$DelData /c "CMD /c del /Q @FILE"
+    $MesssagyBody = $MesssagyBody + ($ZIPSession | Select-Object @{n="Name";e={($_.name).Substring(0, $_.name.LastIndexOf("("))}} ,@{n="Start Time";e={$_.CreationTime}},@{n="End Time";e={$_.EndTime}},Result,@{n="Details";e={($TaskSessions | sort creationtime -Descending | select -first 1).Title}})
+  }
+  
+  }   
+}
+If ($EnableNotification)
+{
+$Message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo
+$Message.Subject = $EmailSubject
+$Message.IsBodyHTML = $True
+$message.Body = $MesssagyBody | ConvertTo-Html -head $style | Out-String
+$SMTP = New-Object Net.Mail.SmtpClient($SMTPServer,$portSMTP)
+#Сервер использует SSL 
+$SMTP.EnableSSL = $true 
+$SMTP.Send($Message)
+}
+
